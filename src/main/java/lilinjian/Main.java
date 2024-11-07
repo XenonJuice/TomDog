e7e23ea997f4ece3994f905ce838c9a37aac079a
xenonJuice <myEmail>
2024-11-07 13:58:59 +0900

    my new work

--- src/main/java/lilinjian/Main.java
+++ src/main/java/lilinjian/Main.java
@@ -3,6 +3,6 @@ package lilinjian;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello excel@!");
+        System.out.println("Hello excel@@!");
     }
 }
