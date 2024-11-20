7af62426368096025bff150a3d36c09c7388570f
xenonJuice <myEmail>
2024-11-20 16:15:53 +0900

    made a note for a certain method of enumerator

--- src/main/java/util/Enumerator.java
+++ src/main/java/util/Enumerator.java
@@ -1,65 +1,4 @@
-/*
- * $Header: /home/cvs/jakarta-tomcat-4.0/catalina/src/share/org/apache/catalina/util/Enumerator.java,v 1.3 2003/04/29 21:57:39 rem Exp $
- * $Revision: 1.3 $
- * $Date: 2003/04/29 21:57:39 $
- *
- * ====================================================================
- *
- * The Apache Software License, Version 1.1
- *
- * Copyright (c) 1999 The Apache Software Foundation.  All rights
- * reserved.
- *
- * Redistribution and use in source and binary forms, with or without
- * modification, are permitted provided that the following conditions
- * are met:
- *
- * 1. Redistributions of source code must retain the above copyright
- *    notice, this list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form must reproduce the above copyright
- *    notice, this list of conditions and the following disclaimer in
- *    the documentation and/or other materials provided with the
- *    distribution.
- *
- * 3. The end-user documentation included with the redistribution, if
- *    any, must include the following acknowledgement:
- *       "This product includes software developed by the
- *        Apache Software Foundation (http://www.apache.org/)."
- *    Alternately, this acknowledgement may appear in the software itself,
- *    if and wherever such third-party acknowledgements normally appear.
- *
- * 4. The names "The Jakarta Project", "Tomcat", and "Apache Software
- *    Foundation" must not be used to endorse or promote products derived
- *    from this software without prior written permission. For written
- *    permission, please contact apache@apache.org.
- *
- * 5. Products derived from this software may not be called "Apache"
- *    nor may "Apache" appear in their names without prior written
- *    permission of the Apache Group.
- *
- * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
- * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE APACHE SOFTWARE FOUNDATION OR
- * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
- * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
- * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
- * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
- * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
- * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
- * SUCH DAMAGE.
- * ====================================================================
- *
- * This software consists of voluntary contributions made by many
- * individuals on behalf of the Apache Software Foundation.  For more
- * information on the Apache Software Foundation, please see
- * <http://www.apache.org/>.
- *
- * [Additional notices, if required by prior licensing conditions]
- *
- */
+
 
 
 package util;
@@ -75,10 +14,10 @@ import java.util.NoSuchElementException;
 
 
 /**
- * Adapter class that wraps an <code>Enumeration</code> around a Java2
- * collection classes object <code>Iterator</code> so that existing APIs
- * returning Enumerations can easily run on top of the new collections.
- * Constructors are provided to easily create such wrappers.
+ * 适配器类，它将 <code>Enumeration</code>
+ * 包装在一个 Java2 集合类对象 <code>Iterator</code> 周围，
+ * 便现有返回 Enumeration 的 API 可以轻松地在新的集合之上运行。
+ * 提供了构造函数以便轻松创建这样的包装器。
  *
  * @author Craig R. McClanahan
  * @version $Revision: 1.3 $ $Date: 2003/04/29 21:57:39 $
@@ -106,7 +45,7 @@ public final class Enumerator implements Enumeration {
      * Return an Enumeration over the values of the specified Collection.
      *
      * @param collection Collection whose values should be enumerated
-     * @param clone true to clone iterator
+     * @param clone      true to clone iterator
      */
     public Enumerator(Collection collection, boolean clone) {
 
@@ -134,7 +73,11 @@ public final class Enumerator implements Enumeration {
      * specified Iterator.
      *
      * @param iterator Iterator to be wrapped
-     * @param clone true to clone iterator
+     * @param clone    true to clone iterator
+     *                 <p>
+     *                 对原Iterator进行克隆会创建一个浅拷贝，主要考虑到多线程环境下，
+     *                 如果调用该类的构造器却不进行拷贝会导致原集合和本类共享一个迭代器，如果其他线程
+     *                 对原集合进行修改，会导致CME异常。
      */
     public Enumerator(Iterator iterator, boolean clone) {
 
@@ -143,10 +86,10 @@ public final class Enumerator implements Enumeration {
             this.iterator = iterator;
         } else {
             List list = new LinkedList();
-            while(iterator.hasNext()) {
+            while (iterator.hasNext()) {
                 list.add(iterator.next());
             }
-            this.iterator = list.iterator();   
+            this.iterator = list.iterator();
         }
 
     }
@@ -167,7 +110,7 @@ public final class Enumerator implements Enumeration {
     /**
      * Return an Enumeration over the values of the specified Map.
      *
-     * @param map Map whose values should be enumerated
+     * @param map   Map whose values should be enumerated
      * @param clone true to clone iterator
      */
     public Enumerator(Map map, boolean clone) {
@@ -194,8 +137,8 @@ public final class Enumerator implements Enumeration {
      * Tests if this enumeration contains more elements.
      *
      * @return <code>true</code> if and only if this enumeration object
-     *  contains at least one more element to provide, <code>false</code>
-     *  otherwise
+     * contains at least one more element to provide, <code>false</code>
+     * otherwise
      */
     public boolean hasMoreElements() {
 
@@ -209,8 +152,7 @@ public final class Enumerator implements Enumeration {
      * has at least one more element to provide.
      *
      * @return the next element of this enumeration
-     *
-     * @exception NoSuchElementException if no more elements exist
+     * @throws NoSuchElementException if no more elements exist
      */
     public Object nextElement() throws NoSuchElementException {
 
